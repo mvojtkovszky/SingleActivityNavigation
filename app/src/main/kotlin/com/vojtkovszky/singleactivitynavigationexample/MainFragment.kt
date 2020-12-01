029c329a45ed5136d2989e2b5afa5b887e8672bb
Marcel Vojtkovszky <marcel.vojtkovszky@smartrefill.se>
2020-12-01 14:16:19 +0100

    fix potential memory leak in example app

--- app/src/main/kotlin/com/vojtkovszky/singleactivitynavigationexample/MainFragment.kt
+++ app/src/main/kotlin/com/vojtkovszky/singleactivitynavigationexample/MainFragment.kt
@@ -10,10 +10,11 @@ import com.vojtkovszky.singleactivitynavigationexample.databinding.FragmentMainB
 
 class MainFragment : BaseSingleFragment() {
 
-    private lateinit var binding: FragmentMainBinding
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentMainBinding.inflate(inflater, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -60,5 +61,10 @@ class MainFragment : BaseSingleFragment() {
             it.text = getString(R.string.back_to_root)
             it.setOnClickListener { navigateBackToRoot() }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
